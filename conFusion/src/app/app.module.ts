66ebe8c8a416006569856c4a59fcd9f67ef86cf5
jasonlu2907@gmail.com <jasonlu2907@gmail.com>
2020-08-17 13:35:29 -0500

    Dialogs

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,29 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
 // MODULES DE DESIGN TRONG HTML
-import { MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
-  MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule,
-  MatSlideToggleModule, MatToolbarModule, MatListModule, MatGridListModule,
-  MatCardModule, MatIconModule, MatProgressSpinnerModule, MatDialogModule } from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { FormsModule } from "@angular/forms";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatDialogModule } from "@angular/material/dialog";
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // ROUTING MODULE
-import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 
 // COMPONENTS
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
+import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AboutComponent } from "./about/about.component";
+import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from "./contact/contact.component";
+import { LoginComponent } from "./login/login.component";
 
 // SERVICES
-import { DishService } from './services/dish.service';
-import { PromotionService } from './services/promotion.service';
-import { LeaderService } from './services/leader.service';
+import { DishService } from "./services/dish.service";
+import { PromotionService } from "./services/promotion.service";
+import { LeaderService } from "./services/leader.service";
 
 @NgModule({
   declarations: [
@@ -34,19 +50,34 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     FlexLayoutModule,
-    MatToolbarModule, MatListModule,
-    MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
-    MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule,
-    MatSlideToggleModule, MatGridListModule, MatCardModule,
-    MatIconModule, MatProgressSpinnerModule, MatDialogModule,
-    AppRoutingModule
+    MatToolbarModule,
+    MatListModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [DishService, PromotionService, LeaderService],
-  bootstrap: [AppComponent]
+  entryComponents: [LoginComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
