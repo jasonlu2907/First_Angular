db7c6639dcdecc06b1a625737bb49795009361f3
jasonlu2907@gmail.com <jasonlu2907@gmail.com>
2020-08-21 23:28:00 -0500

    Promise part2, imagine getting data from server, setTimeOut, using spinner

--- conFusion/src/app/about/about.component.ts
+++ conFusion/src/app/about/about.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Leader } from '../shared/leader';
+import { Leader } from "../shared/leader";
 
-import { LeaderService } from '../services/leader.service';
+import { LeaderService } from "../services/leader.service";
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
 })
 export class AboutComponent implements OnInit {
+  leaders: Leader[];
 
-  leaders: Leader [];
-
-  constructor(private leaderservice: LeaderService) { }
+  constructor(private leaderservice: LeaderService) {}
 
   ngOnInit() {
-    this.leaders = this.leaderservice.getLeaders();
+    // this.leaders = this.leaderservice.getLeaders();
+    this.leaderservice.getLeaders().then((leaders) => (this.leaders = leaders));
   }
-
 }
