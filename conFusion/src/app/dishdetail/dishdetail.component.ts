63de41c0be005cb5582f6866f103199f0938d21c
jasonlu2907@gmail.com <jasonlu2907@gmail.com>
2020-08-22 17:53:42 -0500

    RxJS part2, not so clear params and switchMap module

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,7 @@ import { DishService } from '../services/dish.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -13,21 +14,37 @@ export class DishdetailComponent implements OnInit {
 
   // @Input()
   dish: Dish;
-
+  dishIds: string[];
+  prev: string;
+  next: string;
   constructor(private dishservice: DishService,
               private route: ActivatedRoute,
               private location: Location) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    // this.dish = this.dishservice.getDish(id.toString());
-    this.dishservice.getDish(id.toString())
-                      .subscribe(dish => this.dish = dish); // DUNG OBSERVABLE
-                    // .then(dish => this.dish = dish); // DUNG PROMISE
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params
+          .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+          .subscribe(dish => { 
+                                this.dish = dish; 
+                                this.setPrevNext(dish.id);
+          });
+    
+    // const id = +this.route.snapshot.params['id'];
+    // // this.dish = this.dishservice.getDish(id.toString());
+    // this.dishservice.getDish(id.toString())
+    //                   .subscribe(dish => this.dish = dish); // DUNG OBSERVABLE
+    //                 // .then(dish => this.dish = dish); // DUNG PROMISE
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
 }
