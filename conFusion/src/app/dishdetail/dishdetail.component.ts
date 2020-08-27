181a3b630e8e5b3f9038b43c66800c5ad7695231
jasonlu2907@gmail.com <jasonlu2907@gmail.com>
2020-08-27 17:15:59 -0500

    HTTP part3, saving changes on specific dish using PUTmethod

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -27,6 +27,8 @@ export class DishdetailComponent implements OnInit {
   
   errMess: string;
 
+  dishcopy: Dish;
+
   @ViewChild('cform')commentFormDirective;
 
   constructor(private dishservice: DishService,
@@ -42,7 +44,7 @@ export class DishdetailComponent implements OnInit {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
           .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-          .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+          .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
                               errmess => this.errMess = <any>errmess);
     
     // CŨ
@@ -118,7 +120,12 @@ export class DishdetailComponent implements OnInit {
     this.comment.date = new Date().toISOString();
 
     // push vô dish.comments Array để nó hiện ra luôn ko cần phải adjacentHTML
-    this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this.dishservice.putDish(this.dishcopy)
+      .subscribe(dish => {
+        this.dish = dish; this.dishcopy = dish;
+      },
+      errmess => { this.dish = null; this.dishcopy = null; this.errMess = <any>errmess; });
     this.commentForm.reset({
       author: '',
       rating: 5,
